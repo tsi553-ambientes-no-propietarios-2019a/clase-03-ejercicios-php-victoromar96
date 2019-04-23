047163f9bf88a10e8aba49fe563658797b1b4e85
victoromar96 <victor.espin@epn.edu.ec>
2019-04-22 22:58:16 -0500

    ejercicio 2 subido

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -3,5 +3,23 @@
  * Problema propuesto:
  *
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
+
+
+
 */
+
+	function tituloCentral($variable)
+	{
+		echo "<h1 style = \"text-align:center\">
+		$variable
+		</h1>";
+
+	};
+
+
+	tituloCentral( " Titulo 1 " );
+	echo "<br>";
+	tituloCentral( " Titulo 2 " );
+
+	
 ?>
